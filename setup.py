16277f441c75983e9340b05943a1e24998eb4559
Elvin Yung <elvinyung1@gmail.com>
2015-05-29 20:36:58 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pygress',
+    version='0.0.1',
+    description='Simple commandline progress bars in Python',
+    author='Elvin Yung',
+    author_email='elvin@elvinyung.com',
+    url='https://github.com/elvinyung/pygress'
+)
