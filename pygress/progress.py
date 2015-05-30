520106b80240a5b7f9cb0ed64924deaf57e196fc
Elvin Yung <elvinyung1@gmail.com>
2015-05-29 20:47:35 -0700

    Use stderr by default

--- pygress/progress.py
+++ pygress/progress.py
@@ -4,13 +4,14 @@ import sys
 
 class ProgressBar():
     def __init__(self, length=30, progress=0.0, progchar='#', emptychar='-',
-                 show_percent=False, message=''):
+                 show_percent=False, message='', use_stderr=True):
         self.length = length
         self.progress = progress
         self.progchar = progchar
         self.emptychar = emptychar
         self.show_percent = show_percent
         self.message = message
+        self.out = sys.stderr if use_stderr else sys.stdout
 
     def set_progress(self, progress):
         self.progress = progress
@@ -24,10 +25,10 @@ class ProgressBar():
     def render(self):
         bar_len = min(int(self.progress * self.length), self.length)
         empty_len = self.length - bar_len
-        sys.stdout.write('{} |'.format(self.message))
-        sys.stdout.write(bar_len * self.progchar)
-        sys.stdout.write(empty_len * self.emptychar)
-        sys.stdout.write('| ')
+        self.out.write('{} |'.format(self.message))
+        self.out.write(bar_len * self.progchar)
+        self.out.write(empty_len * self.emptychar)
+        self.out.write('| ')
         if self.show_percent:
-            sys.stdout.write('{}%'.format(int(self.progress * 100)))
-        sys.stdout.write('\r' if self.progress < 1.0 else '\n')
+            self.out.write('{}%'.format(int(self.progress * 100)))
+        self.out.write('\r' if self.progress < 1.0 else '\n')
